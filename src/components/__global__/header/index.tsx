3f102aec8500827b87057e5f0357baeae2217f19
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-04-25 16:53:12 +0200

    add logo to the website

--- src/components/__global__/header/index.tsx
+++ src/components/__global__/header/index.tsx
@@ -2,6 +2,7 @@ import style from './style.module.css'
 import MenuHamburger from '../menuHamburger'
 import { useCallback, useState } from 'react'
 import { Link } from '@tanstack/react-router'
+import Avatar from '../avatar'
 
 const Header = () => {
 
@@ -15,7 +16,10 @@ const Header = () => {
 
   return (
     <header className={style.header}>
-      <div className={style.logo}></div>
+      <div className={style.logo}>
+        <Avatar img={{ src: '/img/logo.jpeg' }} dims={55} />
+        <span style={{ fontWeight: "bold" }}>Wesside</span>
+      </div>
       <nav className={`${style.navbar} ${showMobileMenu && style.active}`}>
         <Link to="/" className={style.navlink} onClick={onNavigation}>Acceuil</Link>
         <Link to="/post/read/[id]" params={{ id: "bfou-dasdfwd-sda" }} className={style.navlink} onClick={onNavigation}>Blog</Link>

26aff5e62975a0a44fe929fcd96609b504d13c1c
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-04-25 16:22:11 +0200

    push files i forgot to stash and commit ... lmao

--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -1,20 +1,54 @@
 import style from './style.module.css'
 import { MoveRight } from 'lucide-react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+
+gsap.registerPlugin(useGSAP)
 
 const LandingHero = () => {
+
+  const sectionRef = useRef<HTMLElement | null>(null);
+  const buttonContainerRef = useRef<HTMLButtonElement | null>(null)
+
+  useGSAP(() => {
+    gsap.from(`.${style.content}`, {
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out",
+    });
+
+    gsap.from(
+      [`.${style.input}`, buttonContainerRef.current],
+      {
+        opacity: 0,
+        y: 20,
+        duration: 0.6,
+        ease: "power2.out",
+        stagger: 0.2,
+        delay: 0.5,
+      }
+    );
+
+  }, { scope: sectionRef });
+
+
   return (
-    <section className={style.section}>
+    <section className={style.section} ref={sectionRef}>
       <div className={style.layer}>
         <div className={style.content}>
           <h1 className={style.big_title}>Salut ! Je suis <span className={style.my__name}>Victor Nzanzu</span>, développeur web et romancier basé en République démocratique du Congo. J'aime partager ce que je connais, ce que j'aime et ce que je pense. Intéressés ? Abonnez-vous à ma newsletter.</h1>
           <form className={style.newsletter__form}>
             <input type="email" placeholder='tupac@gmail.com' className={style.input} />
-            <button type="submit" className={style.button}>
-              <span>S'abonner</span>
-              <span className={`center ${style.icon}`}>
-                <MoveRight size={20} />
-              </span>
-            </button>
+            <span ref={buttonContainerRef}>
+              <button type="submit" className={style.button}>
+                <span>S'abonner</span>
+                <span className={`center ${style.icon}`}>
+                  <MoveRight size={20} />
+                </span>
+              </button>
+            </span>
           </form>
         </div>
       </div>

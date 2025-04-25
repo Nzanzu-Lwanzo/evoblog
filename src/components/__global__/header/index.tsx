56df390bfac103091c72d555e40230389fe4609a
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-04-25 16:33:28 +0200

    fix styling flaws : hero content overflowing from the container (section element because of the div.layer element), add a bigger padding to the article content elements, always set the showMobileMenu to false on navigation so the menu is hidden

--- src/components/__global__/header/index.tsx
+++ src/components/__global__/header/index.tsx
@@ -1,6 +1,6 @@
 import style from './style.module.css'
 import MenuHamburger from '../menuHamburger'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { Link } from '@tanstack/react-router'
 
 const Header = () => {
@@ -11,14 +11,16 @@ const Header = () => {
     setShowMobileMenu(prev => !prev)
   }
 
+  const onNavigation = useCallback(() => setShowMobileMenu(false), [])
+
   return (
     <header className={style.header}>
       <div className={style.logo}></div>
       <nav className={`${style.navbar} ${showMobileMenu && style.active}`}>
-        <Link to="/" className={style.navlink}>Acceuil</Link>
-        <Link to="/post/read/[id]" params={{ id: "bfou-dasdfwd-sda" }} className={style.navlink}>Blog</Link>
-        <Link to="/" className={style.navlink}>Newsletter</Link>
-        <Link to="/" className={style.navlink}>Contacts</Link>
+        <Link to="/" className={style.navlink} onClick={onNavigation}>Acceuil</Link>
+        <Link to="/post/read/[id]" params={{ id: "bfou-dasdfwd-sda" }} className={style.navlink} onClick={onNavigation}>Blog</Link>
+        <Link to="/" className={style.navlink} onClick={onNavigation}>Newsletter</Link>
+        <Link to="/" className={style.navlink} onClick={onNavigation}>Contacts</Link>
       </nav>
       <MenuHamburger showMenu={showMobileMenu} toggleMenuAppearance={toggleMenuAppearance} />
     </header>
--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -35,22 +35,20 @@ const LandingHero = () => {
 
 
   return (
-    <section className={style.section} ref={sectionRef}>
-      <div className={style.layer}>
-        <div className={style.content}>
-          <h1 className={style.big_title}>Salut ! Je suis <span className={style.my__name}>Victor Nzanzu</span>, développeur web et romancier basé en République démocratique du Congo. J'aime partager ce que je connais, ce que j'aime et ce que je pense. Intéressés ? Abonnez-vous à ma newsletter.</h1>
-          <form className={style.newsletter__form}>
-            <input type="email" placeholder='tupac@gmail.com' className={style.input} />
-            <span ref={buttonContainerRef}>
-              <button type="submit" className={style.button}>
-                <span>S'abonner</span>
-                <span className={`center ${style.icon}`}>
-                  <MoveRight size={20} />
-                </span>
-              </button>
-            </span>
-          </form>
-        </div>
+    <section className={`center ${style.section}`} ref={sectionRef}>
+      <div className={style.content}>
+        <h1 className={style.big_title}>Salut ! Je suis <span className={style.my__name}>Victor Nzanzu</span>, développeur web et romancier basé en République démocratique du Congo. J'aime partager ce que je connais, ce que j'aime et ce que je pense. Intéressés ? Abonnez-vous à ma newsletter.</h1>
+        <form className={style.newsletter__form}>
+          <input type="email" placeholder='tupac@gmail.com' className={style.input} />
+          <span ref={buttonContainerRef}>
+            <button type="submit" className={style.button}>
+              <span>S'abonner</span>
+              <span className={`center ${style.icon}`}>
+                <MoveRight size={20} />
+              </span>
+            </button>
+          </span>
+        </form>
       </div>
     </section>
   )

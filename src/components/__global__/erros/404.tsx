a55d6b12275655c55860b8d21ff6bcf81a0d6d0f
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-05-08 15:59:50 +0200

    refactor error handling components to use dynamic contact links for 404 and uncaught errors

--- src/components/__global__/erros/404.tsx
+++ src/components/__global__/erros/404.tsx
@@ -1,11 +1,14 @@
 import style from './style.module.css'
 import ParentErrorComponent from './_parent'
+import { BLOG_OWNER_DATA } from '../../../lib/enums'
+import { mailTo } from './_parent'
+
 
 const PageNotFound = ({ is }: { is: "page" | "ressource" }) => {
     return (
         <ParentErrorComponent title='Erreur 404'>
             Désolé ! La {is} que vous avez requêtée est introuvable ou temporairement indisponible. <br />
-            Si vous pensez que ceci est une erreur, contactez-moi sur mon <a className={style.link} href="tel:0977210519">numéro de téléphone</a> ou sur mon <a className={style.link} href="mailto:nzanzu.lwanzo.work@gmail.com">adresse email</a>.
+            Si vous pensez que ceci est une erreur, contactez-moi sur mon <a className={style.link} href={`tel:${BLOG_OWNER_DATA.PHONE}`}>numéro de téléphone</a> ou sur mon <a className={style.link} href={mailTo('ERREUR 404')}>adresse email</a>.
         </ParentErrorComponent>
     )
 }

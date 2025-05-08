aaa067f0f6a083a66ff7e0862336d5481b9da428
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2025-05-08 15:49:10 +0200

    add error handling components for 404 and uncaught errors, and refactor footer links to use constants

--- src/components/__global__/footer/index.tsx
+++ src/components/__global__/footer/index.tsx
@@ -1,11 +1,12 @@
 import style from './style.module.css'
 import SwitchTheme from '../swicthTheme'
+import { BLOG_OWNER_DATA } from '../../../lib/enums'
 
 const Footer = () => {
     return (
         <footer className={style.footer}>
             <div>
-                <p>Built with love and <a target="_blank" href="https://youtu.be/iykvtX_ptyg?si=Trpl08IF0XktPHQ6" className={style.link__text}>rap music</a> by <a href="https://github.com/Nzanzu-Lwanzo" className={style.link__text}>Nzanzu Lwanzo</a></p>
+                <p>Built with love and <a target="_blank" href="https://youtu.be/iykvtX_ptyg?si=Trpl08IF0XktPHQ6" className={style.link__text}>rap music</a> by <a href={BLOG_OWNER_DATA.GITHUB} className={style.link__text}>Nzanzu Lwanzo</a></p>
             </div>
             <div>
                 <SwitchTheme />
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,32 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import {
+  RouterProvider,
+  createRouter,
+} from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import './assets/css/global.css'
 import './assets/css/theme.css'
+import PageNotFound from './components/__global__/erros/404'
+import UncaughtError from './components/__global__/erros/uncaught'
 
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  scrollRestoration: true,
+  scrollRestorationBehavior: 'smooth',
+  defaultViewTransition: true,
+  trailingSlash: 'never',
+  defaultNotFoundComponent() {
+    return <PageNotFound is='page' />
+  },
+  defaultErrorComponent(props: { error: Error }) {
+    return <>Error : {props.error.message}</>
+  },
+  defaultOnCatch(error, errorInfo) {
+    console.log(error, errorInfo)
+    return <UncaughtError />
+  },
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
